257fe8004afcf97cd63233a0605756924751b350
Clément Renaud <clement.renaud@gmail.com>
2017-07-26 19:06:37 +0200

    Update 2017-07-26T17:06:34.042Z

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Graph from './js/Graph.js'
-import GraphEvent from './js/GraphEvent.js'
-
-// export for browser
-if(typeof window !== 'undefined') window.Graph = Graph
-if(typeof window !== 'undefined') window.GraphEvent = GraphEvent
-
-module.exports.GraphEvent = GraphEvent
-module.exports.Graph = Graph
